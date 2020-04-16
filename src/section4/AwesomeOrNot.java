d0994809cd75b955c2757ee6e6596dece67d9f33
root <root@ca750054f187>
2020-04-16 21:58:52 +0000

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,35 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+    public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+    int random = new Random().nextInt(4) ;
 	// 3. Print out this variable
-
+    System.out.println(random);
 	// 4. Get the user to enter something that they think is awesome
-
+    JOptionPane.showInputDialog(null, "what do you think is awesome?");
 	// 5. If the random number is 0
-
+    if (random== (0)) {
 	// -- tell the user whatever they entered is awesome!
-
+    JOptionPane.showMessageDialog(null, "That is awesome!"); }
 	// 6. If the random number is 1
-
+    if (random== (1)) {
 	// -- tell the user whatever they entered is ok.
-
+    JOptionPane.showMessageDialog(null, "that's just okay."); }
 	// 7. If the random number is 2
-
+    if (random== (2)) {
 	// -- tell the user whatever they entered is boring.
-
+    JOptionPane.showMessageDialog(null, "That's boring!"); }
 	// 8. If the random number is 3
-
+    if (random== (3)) {
 	// -- write your own answer
-
+    JOptionPane.showMessageDialog(null, "Wow!"); }
+}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String one = JOptionPane.showInputDialog(null, "What is the first color of the rainbow?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (one.equals ("red")) {
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		score ++;
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String two = JOptionPane.showInputDialog(null, "What is the first letter of the alphabet? (lowercase letter");
+		if (one.equals ("a")) {
+		score ++;
+		}
+		String three = JOptionPane.showInputDialog(null, "and lastly, the 28 point question, what is 1 + 1?");
+		if (three.equals ("2")) {
+		score += 28;
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		System.out.println(score);
 	}
 }
